9d0e5ad255ab855ebf034a3e2b494a78433eccd1
mandjiny <mandjiny89@gmail.com>
2022-08-30 10:53:39 +0100

    working function created

---
+++ aws_user_func.py
@@ -0,0 +1,29 @@
+import boto3
+iam = boto3.client('iam')
+
+def userCreation():
+    print("printing with open")
+    with open('user_list.txt') as f:
+        users = f.read()
+        users_sorted = users.replace("\n", "")
+
+    existing_user = []
+    for user_check in iam.list_users()['Users']:
+        existing_user_list = ("{0}".format(user_check['UserName']))
+        existing_user += existing_user_list.split(",")
+    print("List of users already exisit", existing_user)
+
+    for user_name in users_sorted.split(","):
+        if user_name not in existing_user:
+            # create a user
+            print("Printing the user name added to the IAM ", user_name)
+            iam.create_user(UserName=user_name)
+            # attach a policy
+            iam.attach_user_policy(
+            UserName =user_name, 
+            PolicyArn='arn:aws:iam::aws:policy/AmazonEC2FullAccess'
+            )
+        else:
+            print("User already exit", user_name)
+
+userCreation()
